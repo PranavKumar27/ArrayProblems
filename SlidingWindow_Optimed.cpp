6f85ee65242a73b5b4c6e802f1904daabab281ba
PranavKumar27 <pranav.jenus@gmail.com>
2021-09-06 08:09:00 +0530

    Update SlidingWindow_Optimed.cpp

--- SlidingWindow_Optimed.cpp
+++ SlidingWindow_Optimed.cpp
@@ -1,1 +1,67 @@
+#include <iostream>
+#include <vector>
 
+using namespace std;
+
+// Numbers are 1 2 3 4 5 2 3 5 6 2 3 4 1 5
+// Find all the sets of numbers whose sum is k. where k=8
+// 3 5
+// 6 2
+// 3 4 1
+#define K 10
+
+vector<int> Result;
+
+void subSetSumEqualsK(vector<int> v)
+{
+	int i,j;
+	int n;
+	
+	n=v.size();
+	int sum=0;
+	j=0;
+	for(i=0;i<n && j<n;)
+	{
+		//sum=0;
+		while(sum<K && sum!=K)
+		{
+			sum+=v[j];
+			j++;
+		}
+		while(sum>K)
+		{
+			sum-=v[i];
+			i++;
+		}
+		if(sum==K)
+		{
+			Result.push_back(i);
+			Result.push_back(j-1);
+			vector<int> out(v.begin()+i,v.begin()+j);
+				
+				for(auto m:out)
+				{
+					cout << m << ",";
+				}
+				cout << endl;
+		}
+		sum+=v[j];
+		j++;
+	}
+	cout << "Result in indexs in vector=" << endl;
+	
+	for(auto q:Result)
+	{
+		cout << q << ",";
+	}
+	cout << endl;
+}
+
+int main(int argc, char *argv[])
+{
+	vector<int> v= {1,2,3,4,5,2,3,5,6,2,3,4,1,5};
+	
+	subSetSumEqualsK(v);
+	
+	return 0;	
+}
